042fd6f88ee60b3d07222663e508b3845c308d96
Lukasz <lukaszskuzjus@gmail.com>
2017-08-24 17:29:57 +0200

    ok

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//var name = prompt('Enter your name');
+//alert('Hello, ' + name);
+//console.log('Hello, ' + name);
+
+/*var f = [];
+function factorial (n) {
+  if (n == 0 || n == 1)
+    return 1;
+  if (f[n] > 0)
+    return f[n];
+  return f[n] = factorial(n-1) * n;
+}*/
